352706f8815e9fa9ff4a7c3d82d11d208ab2c045
breity55 <breitensa23@outlook.com>
2019-04-28 11:03:37 -0500

    search

---
+++ beer-search.js
@@ -0,0 +1,21 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+    const params = {
+        TableName: "Beer",
+        KeyConditionExpression: "userId = :userId",
+        FilterExpression: "contains (breweryName , :searchQuery) or contains (beerName, :searchQuery)",
+        ExpressionAttributeValues: {
+            ":userId": event.requestContext.identity.cognitoIdentityId,
+            ":searchQuery": event.pathParameters.query,
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("query", params);
+        return success(result.Items);
+    } catch (e) {
+        return failure({ status: false, e });
+    }
+}
