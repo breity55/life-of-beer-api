fcab2fb5cb5524fd67b1a9e3e5377ab5c7f44839
breity55 <breitensa23@outlook.com>
2020-01-07 19:29:58 -0600

    ability to add multiple beers

---
+++ beer-create-list.js
@@ -0,0 +1,50 @@
+import uuid from "uuid";
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+export async function main(event, context, callback) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: "Beer",
+        requestorId: event.requestContext.identity.cognitoIdentityId
+    };
+
+    for (var i = 0; i < data.length; i++) {
+        var newRecordParams = {
+            TableName: params.TableName,
+            Item: {
+                userId: params.requestorId,
+                beerId: uuid.v1(),
+                breweryName: data[i].breweryName,
+                breweryLocation: data[i].breweryLocation,
+                beerName: data[i].beerName,
+                beerStyle: data[i].beerStyle,
+                beerNotes: data[i].beerNotes,
+                approvedIndicator: data[i].approvedIndicator,
+                favoriteIndicator: data[i].favoriteIndicator,
+                createdDate: Date.now()
+            }
+        }
+
+        const checkIfRecordExistsParams = {
+            TableName: newRecordParams.TableName,
+            KeyConditionExpression: "userId = :userId",
+            FilterExpression: "contains (breweryName , :searchBreweryName) and contains (beerName, :searchBeerName) and contains (breweryLocation, :searchBreweryLocation)",
+            ExpressionAttributeValues: {
+                ":userId": newRecordParams.Item.userId,
+                ":searchBreweryName": newRecordParams.Item.breweryName,
+                ":searchBreweryLocation": newRecordParams.Item.breweryLocation,
+                ":searchBeerName": newRecordParams.Item.beerName
+            }
+        };
+
+        const result = await dynamoDbLib.call("query", checkIfRecordExistsParams);
+        if (result.Items.length <= 0) {
+            try {
+                await dynamoDbLib.call("put", newRecordParams);
+            } catch (e) {
+                return failure({ status: false, message: e.message });
+            }
+        }
+    }
+}
